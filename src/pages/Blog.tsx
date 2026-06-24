4cb7e3326c400a1160429f4f220dee08f0b1f618
souzalrns <souzalrns@gmail.com>
2026-06-24 16:50:16 +0100

    blog: importar de allBlogPosts.ts (ficheiro consolidado com 82 artigos)

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -7,7 +7,7 @@ import { ArrowRight, Calendar, Clock, User, TrendingUp, FileText, Scale, Heart, 
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
-import { allBlogPosts as blogPosts } from '@/data/blogPosts';
+import { allBlogPosts as blogPosts } from '@/data/allBlogPosts';
 
 
 export default function Blog() {
